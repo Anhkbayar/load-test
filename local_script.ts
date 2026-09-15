2368489c02a0b0d6f9d406258ed69d415fb9bcc5
Ankhbayar <zanhaa56@gmail.com>
2026-09-15 20:57:48 +0800

    300ms iin delaytai local 100vu tai test

--- local_script.ts
+++ local_script.ts
@@ -4,7 +4,7 @@ import http from 'k6/http';
 export const options = { vus: 100, duration: "1m" };
 
 export default function () {
-  const res = http.get('http://localhost:3000');
+  const res = http.get('http://localhost:3000/delay');
   check(res, { 'status 200 байна': (r) => r.status === 200 });
   sleep(1);
 }
