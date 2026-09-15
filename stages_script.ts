d10c168dd58fa95c8c653d1058fbef96704884b3
Ankhbayar <zanhaa56@gmail.com>
2026-09-15 15:57:13 +0800

    negtgesen stages

--- stages_script.ts
+++ stages_script.ts
@@ -1,7 +1,15 @@
 import { check, sleep } from 'k6';
 import http from 'k6/http';
 
-export const options = { vus: 5, duration: "30s" };
+export const options = {
+  stages: [
+    { duration: '30s', target: 5 }, // халаалт
+    { duration: '1m', target: 30 }, // өсгөлт
+    { duration: '30s', target: 100 }, // оргил
+    { duration: '30s', target: 0 }, // буулт
+  ],
+
+};
 
 export default function () {
   const res = http.get('https://test.k6.io');
