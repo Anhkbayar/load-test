0404f2c4cd932373d575f43c925be055a76117c7
Ankhbayar <zanhaa56@gmail.com>
2026-09-15 20:53:22 +0800

    local deer 30 vu tai test

--- local_script.ts
+++ local_script.ts
@@ -1,7 +1,7 @@
 import { check, sleep } from 'k6';
 import http from 'k6/http';
 
-export const options = { vus: 30, duration: "1m" };
+export const options = { vus: 100, duration: "1m" };
 
 export default function () {
   const res = http.get('http://localhost:3000');
