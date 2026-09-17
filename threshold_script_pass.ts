9a65dc4900d8b8f4c001780be2d425c4bd253725
Ankhbayar <zanhaa56@gmail.com>
2026-09-18 00:57:20 +0800

    threshold test bolon dugnelt

--- threshold_script_pass.ts
+++ threshold_script_pass.ts
@@ -2,7 +2,7 @@ import { check, sleep } from 'k6';
 import http from 'k6/http';
 
 export const options = {
-  vus: 5, duration: "1m",
+  vus: 30, duration: "1m",
   thresholds: {
     http_req_duration: ['p(95)<344'],
     http_req_failed: ['rate<0.001'], // SLO: error rate < 0.1%
